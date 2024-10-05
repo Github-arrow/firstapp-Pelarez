897de74e2700b5570a26f09218ba5d9e2564567b
Brian <brian.d.pealrez@outlook.ph>
2024-10-05 11:56:32 +0800

    upddate

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/',[PelarezController::class, "personalinfo"]);
+Route::get('/personalinfo',[PelarezController::class, "personalinfo"]);
